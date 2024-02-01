3c4364a7f8cc84cad1a146ea71f5de30fff18518
Choihyeongu <a11dsd123@gmail.com>
2024-02-01 19:43:11 +0900

    add: template.config.js 추가

--- template.config.js
+++ template.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  // Placeholder used to rename and replace in files
-  // package.json, index.json, android/, ios/
-  placeholderName: "ReactNativeAble",
+  placeholderName: 'ProjectName',
 
-  // Directory with template
-  templateDir: "./template",
+  titlePlaceholder: `Able's Template`,
 
-  // Path to script, which will be executed after init
-  postInitScript: "./script.js"
+  templateDir: './template',
+
+  postInitScript: './script.js',
 };
